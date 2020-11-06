847dc714fe5af8c76630e8b89f9788314f335b2e
Kuba Wojtkowicz <J.WOJTKOWICZ1@nuigalway.ie>
2020-11-06 18:13:05 +0000

    adding method for signing up students to a course and updating console printing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,69 +1,235 @@
-import com.sun.tools.javac.util.List;
+
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+
 
 public class Main {
 
+    // Creating students
+    private static final Student student1 = new Student("Mary Ham", 20, "01/06/2000", 16365972L);
+    private static final Student student2 = new Student("John Smith", 22, "09/08/1998", 86739684L);
+    private static final Student student3 = new Student("Paul Baker", 21, "24/12/1998", 26542867L);
+    private static final Student student4 = new Student("Peter Black", 22, "03/04/1998", 29367418L);
+    private static final Student student5 = new Student("Amy Turner", 22, "28/05/1998", 52009569L);
+    private static final Student student6 = new Student("Zack Wheel", 20, "17/11/1999", 10376423L);
+    private static final Student student7 = new Student("Sarah Yale", 21, "21/01/1999", 85746277L);
+    private static final Student student8 = new Student("Michael Love", 23, "19/04/1997", 55493658L);
+    private static final Student student9 = new Student("Oran Apple", 22, "02/11/1997", 73953765L);
+    private static final Student student10 = new Student("Rachel Red", 22, "12/01/1998", 23498832L);
+
+    // Creating modules
+    private static final Module module1 = new Module("Software Engineering", "CT417");
+    private static final Module module2 = new Module("Machine Learning", "CT4101");
+    private static final Module module3 = new Module("Artificial Intelligence", "CT421");
+    private static final Module module4 = new Module("Circuits", "E224");
+    private static final Module module5 = new Module("Math", "E861");
+
+    // Creating Academic start and end date
+    private static final DateTime dateTime1 = new DateTime(2020, 9, 1, 10, 0);
+    private static final DateTime dateTime2 = new DateTime(2021, 6, 20, 16, 30);
+
+    // Creating courses
+    private static final CourseProgramme courseProgramme1 = new CourseProgramme("CS & IT", dateTime1, dateTime2);
+    private static final CourseProgramme courseProgramme2 = new CourseProgramme("ECE", dateTime1, dateTime2);
+
+    // Creating Lists of courses, modules and students
+    private static List<CourseProgramme> courses;
+    private static List<Module> modules;
+    private static List<Student> students;
+
     public static void main(String args[]) {
 
-        // Creating students to populate modules
-        Student s1 = new Student("Mary Ham", 20, "01/06/2000", 16365972L);
-        Student s2 = new Student("John Smith", 22, "09/08/1998", 86739684L);
-        Student s3 = new Student("Paul Baker", 21, "24/12/1998", 26542867L);
-        Student s4 = new Student("Peter Black", 22, "03/04/1998", 29367418L);
-        Student s5 = new Student("Amy Turner", 22, "28/05/1998", 52009569L);
-        Student s6 = new Student("Zack Wheel", 20, "17/11/1999", 10376423L);
-        Student s7 = new Student("Sarah Yale", 21, "21/01/1999", 85746277L);
-        Student s8 = new Student("Michael Love", 23, "19/04/1997", 55493658L);
-        Student s9 = new Student("Oran Apple", 22, "02/11/1997", 73953765L);
-        Student s10 = new Student("Rachel Red", 22, "12/01/1998", 23498832L);
+        // Creating student, module and course data
+        setUp();
 
-        // Creating modules to populate course programme
-        Module m1 = new Module("Software Engineering", "CT417", List.of(s1, s2, s3, s4, s5));
-        Module m2 = new Module("Machine Learning", "CT4101", List.of(s1, s2, s3, s4, s5, s6, s7, s10));
-        Module m3 = new Module("Artificial Intelligence", "CT421", List.of(s1, s2, s3, s5, s6, s7, s10));
-        Module m4 = new Module("Circuits", "E224", List.of(s6, s7, s8, s9, s10));
-        Module m5 = new Module("Math", "E861", List.of(s6, s7, s8, s9, s10));
+        // Printing Data to console
+        printCourseData();
+        printModuleData();
+        printStudentData();
+    }
 
-        // Creating Academic start and end date
-        DateTime dt1 = new DateTime(2020, 9, 1, 10, 0);
-        DateTime dt2 = new DateTime(2021, 6, 20, 16, 30);
+    private static void setUp() {
 
-        // Creating courses
-        CourseProgramme cp1 = new CourseProgramme("CS & IT", List.of(m1, m2, m3), dt1, dt2);
-        CourseProgramme cp2 = new CourseProgramme("ECE", List.of(m2, m3, m4, m5), dt1, dt2);
+        // Populating Lists of available courses, modules and students
+        courses = List.of(courseProgramme1, courseProgramme2);
+        modules = List.of(module1, module2, module3, module4, module5);
+        students = List.of(student1, student2, student3, student4, student5, student6, student7, student8, student9, student10);
 
-        // Creating Lists of courses, modules and students
-        List<CourseProgramme> courses = List.of(cp1, cp2);
-        List<Module> modules = List.of(m1, m2, m3, m4, m5);
-        List<Student> students = List.of(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10);
+        // Setting Course Modules
+        courseProgramme1.setModules(List.of(module1, module2, module3));
+        courseProgramme2.setModules(List.of(module2, module3, module4, module5));
 
-        // Printing data to console
-        // Printing Course data given a list of courses
-        System.out.println("\n\nCOURSES: \n" );
-        courses.stream().forEach(course -> System.out.println(
-                "Name: " + course.getName() +
-                "\nModules: " + course.getModules().stream().map(Module::getName).collect(Collectors.toList()) +
-                "\nAcademic Start Date: " + course.getAcademicStartDate() +
-                "\nAcademic End Date: " + course.getAcademicEndDate() +
-                "\n"));
+        // Signing up students to the courses
+        signUpStudentToCourse(student1, courseProgramme1);
+        signUpStudentToCourse(student2, courseProgramme1);
+        signUpStudentToCourse(student3, courseProgramme1);
+        signUpStudentToCourse(student4, courseProgramme1);
+        signUpStudentToCourse(student5, courseProgramme1);
+        signUpStudentToCourse(student6, courseProgramme2);
+        signUpStudentToCourse(student7, courseProgramme2);
+        signUpStudentToCourse(student8, courseProgramme2);
+        signUpStudentToCourse(student9, courseProgramme2);
+        signUpStudentToCourse(student10, courseProgramme2);
+    }
 
-        // Printing Module data given a list of modules
-        System.out.println("\n\nMODULES: \n" );
-        modules.stream().forEach(module -> System.out.println(
-                "Name: " + module.getName() +
-                "\nID: " + module.getID() +
-                "\nStudents Enrolled: " + module.getStudentsEnrolled().stream().map(Student::getName).collect(Collectors.toList()) +
-                "\n"));
+    // Adds a student to a course and its modules, saves the course to the student record
+    private static void signUpStudentToCourse(final Student student, final CourseProgramme courseProgramme) {
+        List<CourseProgramme> requiredCourseList = courses.stream().filter(c -> c.getName().equals(courseProgramme.getName())).collect(Collectors.toList());
+        List<Student> requiredStudentList = students.stream().filter(s -> s.getName().equals(student.getName())).collect(Collectors.toList());
+        CourseProgramme currentCourse = requiredCourseList.get(0);
+        Student currentStudent = requiredStudentList.get(0);
+        List<Module> currentCourseModules = currentCourse.getModules();
+        List<CourseProgramme> studentCourses = currentStudent.getRegisteredCourses();
+        List<Module> currentStudentModules = currentStudent.getRegisteredModules();
 
-        // Printing Module data given a list of modules
-        System.out.println("\n\nSTUDENTS: \n" );
-        students.stream().forEach(student -> System.out.println(
-                "Name: " + student.getName() +
-                "\nAge: " + student.getAge() +
-                "\nDOB: " + student.getDOB() +
-                "\nID: " + student.getID() +
-                "\n"));
+        // Add the student to the course
+        List<Student> courseStudents = currentCourse.getStudentsEnrolled();
+        List<Student> updatedCourseStudents = new ArrayList<>();
+        if (courseStudents != null && !courseStudents.contains(student)) {
+            updatedCourseStudents.addAll(courseStudents);
+            updatedCourseStudents.add(student);
+            currentCourse.setStudentsEnrolled(updatedCourseStudents);
+        } else if (courseStudents == null) {
+            updatedCourseStudents.add(student);
+            currentCourse.setStudentsEnrolled(updatedCourseStudents);
+        }
+
+        // Add student to the course modules
+        if (currentCourseModules != null) {
+            currentCourseModules.stream().forEach(m -> {
+                List<Student> updatedModuleStudents = new ArrayList<>();
+                if (m.getStudentsEnrolled() != null && !m.getStudentsEnrolled().contains(student)) {
+                    updatedModuleStudents.addAll(m.getStudentsEnrolled());
+                    updatedModuleStudents.add(student);
+                    m.setStudentsEnrolled(updatedModuleStudents);
+                } else if (m.getStudentsEnrolled() == null) {
+                    updatedModuleStudents.add(student);
+                    m.setStudentsEnrolled(updatedModuleStudents);
+                }
+            });
+        }
+
+        // Add course to student record
+        List<CourseProgramme> updatedStudentCourses = new ArrayList<>();
+        if (studentCourses != null && !studentCourses.contains(courseProgramme)) {
+            updatedStudentCourses.addAll(studentCourses);
+            updatedStudentCourses.add(courseProgramme);
+            currentStudent.setRegisteredCourses(updatedStudentCourses);
+        } else if (studentCourses == null) {
+            updatedStudentCourses.add(courseProgramme);
+            currentStudent.setRegisteredCourses(updatedStudentCourses);
+        }
+
+        // Add course modules to the student record
+        List<Module> updatedStudentModules = new ArrayList<>();
+        if (currentStudentModules != null) {
+            updatedStudentModules.addAll(currentStudentModules);
+            currentCourseModules.stream().forEach(m ->
+            {
+                if (!currentStudentModules.contains(m)) {
+                    updatedStudentModules.add(m);
+                }
+            });
+        } else if (studentCourses == null && currentCourseModules != null) {
+            updatedStudentModules.addAll(currentCourseModules);
+            currentStudent.setRegisteredModules(updatedStudentModules);
+        }
+
+        // Add course to the course module
+        currentCourseModules.stream().forEach(m -> {
+            List<CourseProgramme> updatedModuleCourses = new ArrayList<>();
+            if (m.getCourseProgrammes() != null && !m.getCourseProgrammes().contains(courseProgramme)) {
+                updatedModuleCourses.addAll(m.getCourseProgrammes());
+                updatedModuleCourses.add(courseProgramme);
+                m.setCourseProgrammes(updatedModuleCourses);
+            } else if (m.getCourseProgrammes() == null) {
+                updatedModuleCourses.add(courseProgramme);
+                m.setCourseProgrammes(updatedModuleCourses);
+            }
+        });
+    }
+
+    // Printing all course data
+    private static void printCourseData() {
+        if (!courses.isEmpty()) {
+            // Printing Course data given a list of courses
+            System.out.println("\n\nCOURSES:");
+            courses.stream().forEach(course ->
+            {
+                System.out.println("\nCourse Name: " + course.getName() +
+                        "\nAcademic Start Date: " + course.getAcademicStartDate() +
+                        "\nAcademic End Date: " + course.getAcademicEndDate());
+                if (course.getModules() != null) {
+                    System.out.println("Modules: " + course.getModules().stream().map(Module::getName).collect(Collectors.toList()));
+                } else {
+                    System.out.println("Modules: NO REGISTERED MODULES");
+                }
+                if (course.getStudentsEnrolled() != null) {
+                    System.out.println("Students: " + course.getStudentsEnrolled().stream().map(Student::getName).collect(Collectors.toList()));
+                } else {
+                    System.out.println("Students: NO REGISTERED STUDENTS");
+                }
+
+            });
+        } else {
+            System.out.println("NO REGISTERED COURSES");
+        }
+    }
+
+    // Printing all module data
+    private static void printModuleData() {
+        if (!modules.isEmpty()) {
+            // Printing Module data given a list of modules
+            System.out.println("\n\nMODULES:");
+            modules.stream().forEach(module ->
+            {
+                System.out.println("\nModule Name: " + module.getName() +
+                        "\nID: " + module.getID());
+                if (module.getStudentsEnrolled() != null) {
+                    System.out.println("Students: " + module.getStudentsEnrolled().stream().map(Student::getName).collect(Collectors.toList()));
+                } else {
+                    System.out.println("Students: NO REGISTERED STUDENTS");
+                }
+                if (module.getCourseProgrammes() != null) {
+                    System.out.println("Courses: " + module.getCourseProgrammes().stream().map(CourseProgramme::getName).collect(Collectors.toList()));
+                } else {
+                    System.out.println("Courses: NO REGISTERED COURSES");
+                }
+
+            });
+        } else {
+            System.out.println("NO REGISTERED MODULES");
+        }
+    }
+
+    // Printing all student data
+    private static void printStudentData() {
+        if (!students.isEmpty()) {
+            // Printing Module data given a list of modules
+            System.out.println("\n\nSTUDENTS:");
+            students.stream().forEach(student ->
+            {
+                System.out.println(
+                        "\nStudent Name: " + student.getName() +
+                                "\nAge: " + student.getAge() +
+                                "\nDOB: " + student.getDOB() +
+                                "\nID: " + student.getID());
+                if (student.getRegisteredModules() != null) {
+                    System.out.println("Modules: " + student.getRegisteredModules().stream().map(Module::getName).collect(Collectors.toList()));
+                } else {
+                    System.out.println("Modules: NO REGISTERED MODULES");
+                }
+                if (student.getRegisteredCourses() != null) {
+                    System.out.println("Courses: " + student.getRegisteredCourses().stream().map(CourseProgramme::getName).collect(Collectors.toList()));
+                } else {
+                    System.out.println("Courses: NO REGISTERED COURSES");
+                }
+            });
+        } else {
+            System.out.println("NO STUDENTS REGISTERED");
+        }
     }
 }
